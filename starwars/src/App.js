4ced0763ba3a1d284e0c720a36893d8f915d35ab
Elijah Paminsan <e.paminsan@gmail.com>
2019-08-02 19:57:35 -0700

    Set up Sass file

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,22 +13,32 @@ const App = () => {
   // sync up with, if any.
   useEffect(() => {
     axios
-      .get('https://swapi.co/api/people/')
+      .get("https://swapi.co/api/people/")
       .then(info => {
-        setData(info.data.results)
-        console.log(info.data.results)
+        setData(info.data.results);
+        console.log(info.data.results);
       })
       .catch(err => {
-        console.log("Error: ", err)
-      })
+        console.log("Error: ", err);
+      });
   }, []);
-
-
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {Array.from(data).map(char => (<Character name={char.name} gender={char.gender} birth_year={char.birth_year}/>))}
+      <div className='cardContainer'>
+      {Array.from(data).map(char => (
+        <Character
+          name={char.name}
+          height={char.height}
+          mass={char.mass}
+          hair_color={char.hair_color}
+          eye_color={char.eye_color}
+          birth_year={char.birth_year}
+          gender={char.gender}
+        />
+      ))}
+      </div>
     </div>
   );
 };
--- starwars/src/components/character.js
+++ starwars/src/components/character.js
@@ -2,11 +2,29 @@ import React from "react";
 
 export default function Character(props) {
   return (
-    <div>
-      <h3 class='name'>{props.name}</h3>
-      <p>Gender: {props.gender}</p>
-      <p>{props.species}</p>
-      <p>{props.birth_year}</p>
+    <div className="character">
+      <h3 className="name">{props.name}</h3>
+      <p>
+        <b>Height:</b> {props.height}
+      </p>
+      <p>
+        <b>Mass:</b> {props.mass}
+      </p>
+      <p>
+        <b>Hair Color:</b> {props.hair_color}
+      </p>
+      <p>
+        <b>Skin Color:</b> {props.skin_color}
+      </p>
+      <p>
+        <b>Eye Color:</b> {props.eye_color}
+      </p>
+      <p>
+        <b>Year of Birth:</b> {props.birth_year}
+      </p>
+      <p>
+        <b>Gender:</b> {props.gender}
+      </p>
     </div>
   );
 }
